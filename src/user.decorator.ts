59d2f090fa889fc9f87cf905ac4c052d1cf3d844
Gabriel Marques Dalla Costa <gibrel@gmail.com>
2021-03-24 14:37:24 -0300

    comment clean up :bulb:

--- src/user.decorator.ts
+++ src/user.decorator.ts
@@ -3,6 +3,6 @@ export interface User { id: string; }
 import { createParamDecorator } from '@nestjs/common';
 
 export const User = createParamDecorator((data, req) => {
-    const user: User = req.user || { id: 'todo: use auth' }; // todo use http://www.passportjs.org/ or so
+    const user: User = req.user || { id: 'todo: use auth' };
     return user;
 });
